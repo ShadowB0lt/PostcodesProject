ee7659eeef5771b1ed69cc1b07e05c2e52bf9231
Samuel Jegede <SJegede@spartaglobal.com>
2019-04-24 11:25:10 +0100

    closed the file for file reader and json parser

--- src/main/java/com/sparta/eng_28/PostcodeJsonParser.java
+++ src/main/java/com/sparta/eng_28/PostcodeJsonParser.java
@@ -15,6 +15,7 @@ public class PostcodeJsonParser {
             PostcodeFileReader reader = new PostcodeFileReader(filepath);
             JSONParser parser = new JSONParser();
             postcodeJson = (JSONObject) parser.parse(reader.getPostcodeFile());
+            reader.closePostcodeFile();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
